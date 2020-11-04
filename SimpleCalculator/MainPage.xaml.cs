17209a7717c69e8ee39f3c677b89852c272093c0
Alberto <betoglez.1902@gmail.com>
2020-11-04 00:56:18 -0600

    Complete functionality implementing MVVM pattern

--- SimpleCalculator/MainPage.xaml.cs
+++ SimpleCalculator/MainPage.xaml.cs
@@ -23,151 +23,17 @@ namespace SimpleCalculator
     /// </summary>
     /// 
 
-    enum Operation { Add, Subtract, Multiply, Divide, Equal }
-
     public sealed partial class MainPage : Page
     {
+        /*
         public double op1 = 0.0;
         public double op2 = 0.0;
         public string displayNumber = "";
         private Operation? currOperation;
-
+        */
         public MainPage()
         {
             this.InitializeComponent();
-            // Set the app window preferred launch view size
-            ApplicationView.PreferredLaunchViewSize = new Size(Height = 480, Width = 270);
-            // Set app window preferred launch windowing mode
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-        }
-        private void SetNumberValue(String number)
-        {
-            displayNumber += number;
-            this.SetResultText(double.Parse(displayNumber));
-        }
-        private void SetResultText(double number)
-        {
-            resultTxtBlock.Text = number.ToString();
-        }
-        private void SetOperation(Operation operation)
-        {
-            if (!string.IsNullOrEmpty(displayNumber))
-            {
-                if (op1 == 0.0)
-                {
-                    op1 = double.Parse(displayNumber);
-                }
-                else
-                {
-                    op2 = double.Parse(displayNumber);
-                    op1 = this.PerformOperation(op1, op2, currOperation);
-                    this.SetResultText(op1);
-                }
-            }
-            currOperation = operation;
-            displayNumber = "";
-        }
-        private double PerformOperation(double op1, double op2, Operation? operation)
-        {
-            double result = 0.0;
-            switch (operation)
-            {
-                case Operation.Add:
-                    result = op1 + op2;
-                    break;
-                case Operation.Subtract:
-                    result = op1 - op2;
-                    break;
-                case Operation.Multiply:
-                    result = op1 * op2;
-                    break;
-                case Operation.Divide:
-                    result = op1 / op2;
-                    break;
-                case Operation.Equal:
-                    result = op1;
-                    break;
-            }
-            return result;
-        }
-        private void Btn7_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("7");
-        }
-        private void Btn8_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("8");
-        }
-        private void Btn9_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("9");
-        }
-        private void Btn4_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("4");
-        }
-        private void Btn5_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("5");
-        }
-        private void Btn6_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("6");
-        }
-        private void Btn1_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("1");
-        }
-        private void Btn2_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("2");
-        }
-        private void Btn3_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("3");
-        }
-        private void Btn0_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetNumberValue("0");
-        }
-        private void PointBtn_Click(object sender, RoutedEventArgs e)
-        {
-        }
-        private void CBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.currOperation = null;
-            this.displayNumber = "";
-            this.op1 = 0.0;
-            this.op2 = 0.0;
-            this.SetResultText(0);
-        }
-        private void CeBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.displayNumber = "";
-            this.SetResultText(0);
-        }
-        private void ModBtn_Click(object sender, RoutedEventArgs e)
-        {
-        }
-        private void DivideBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetOperation(Operation.Divide);
-        }
-        private void MultBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetOperation(Operation.Multiply);
-        }
-        private void MinusBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetOperation(Operation.Subtract);
-        }
-        private void PlusBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetOperation(Operation.Add);
-        }
-        private void EqualBtn_Click(object sender, RoutedEventArgs e)
-        {
-            this.SetOperation(Operation.Equal);
         }
     }
 }
---
+++ SimpleCalculator/MainViewModel.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleCalculator
+{
+    enum Operation { Add, Subtract, Multiply, Divide, Equal }
+    class MainViewModel : MainViewModelBase
+    {
+        private readonly string POINT_CHARACTER = ".";
+        private double op1 = 0.0;
+        private double op2 = 0.0;
+        private string displayNumber = "0";
+        private string currentResult = "";
+        private Operation? currOperation;
+
+        public string DisplayNumber
+        {
+            get
+            {
+                return displayNumber;
+            }
+            set
+            {
+                displayNumber = value;
+                OnPropertyChanged("DisplayNumber");
+            }
+        }
+
+        public ICommand SelectButton
+        {
+            get
+            {
+                return new DelegateCommand(param => CheckButtonSelection(param), true);
+            }
+        }
+
+        private void CheckButtonSelection(object param)
+        {
+            string selectedBtn = param.ToString();
+            switch (selectedBtn)
+            {
+                case "Number0":
+                    SetNumberValue("0");
+                    break;
+                case "Number1":
+                    SetNumberValue("1");
+                    break;
+                case "Number2":
+                    SetNumberValue("2");
+                    break;
+                case "Number3":
+                    SetNumberValue("3");
+                    break;
+                case "Number4":
+                    SetNumberValue("4");
+                    break;
+                case "Number5":
+                    SetNumberValue("5");
+                    break;
+                case "Number6":
+                    SetNumberValue("6");
+                    break;
+                case "Number7":
+                    SetNumberValue("7");
+                    break;
+                case "Number8":
+                    SetNumberValue("8");
+                    break;
+                case "Number9":
+                    SetNumberValue("9");
+                    break;
+                case "CBtn":
+                    ClearAll();
+                    break;
+                case "CeBtn":
+                    ClearPartial();
+                    break;
+                case "modBtn":
+                    CalculateMod();
+                    break;
+                case "divideBtn":
+                    SetOperation(Operation.Divide);
+                    break;
+                case "multBtn":
+                    SetOperation(Operation.Multiply);
+                    break;
+                case "minusBtn":
+                    SetOperation(Operation.Subtract);
+                    break;
+                case "plusBtn":
+                    SetOperation(Operation.Add);
+                    break;
+                case "pointBtn":
+                    AddPoint();
+                    break;
+                case "equalBtn":
+                    SetOperation(Operation.Equal);
+                    break;
+            }
+        }
+        private void SetNumberValue(String number)
+        {
+            currentResult += number;
+            DisplayNumber = currentResult;
+        }
+        private void SetOperation(Operation operation)
+        {
+            if (!string.IsNullOrEmpty(currentResult))
+            {
+                if (op1 == 0.0)
+                {
+                    op1 = double.Parse(currentResult);
+                }
+                else
+                {
+                    op2 = double.Parse(currentResult);
+                    op1 = PerformOperation(op1, op2, currOperation);
+                    DisplayNumber = op1.ToString();
+                }
+            }
+            currOperation = operation;
+            currentResult = "";
+        }
+        private double PerformOperation(double op1, double op2, Operation? operation)
+        {
+            double result = 0.0;
+            switch (operation)
+            {
+                case Operation.Add:
+                    result = op1 + op2;
+                    break;
+                case Operation.Subtract:
+                    result = op1 - op2;
+                    break;
+                case Operation.Multiply:
+                    result = op1 * op2;
+                    break;
+                case Operation.Divide:
+                    result = op1 / op2;
+                    break;
+                case Operation.Equal:
+                    result = op1;
+                    break;
+            }
+            return result;
+        }
+        private void CalculateMod()
+        {
+            if (op1 == 0.0 && !string.IsNullOrEmpty(currentResult))
+            {
+                op1 = double.Parse(currentResult);
+            }
+            double altOpResult = op1 / 100;
+            DisplayNumber = altOpResult.ToString();
+            op1 = altOpResult;
+            currOperation = null;
+            currentResult = "";
+        }
+        private void AddPoint()
+        {
+            if (!string.IsNullOrEmpty(currentResult) && !currentResult.Contains(POINT_CHARACTER))
+            {
+                currentResult += POINT_CHARACTER;
+                DisplayNumber = currentResult;
+            }
+        }
+        private void ClearAll()
+        {
+            currOperation = null;
+            currentResult = "";
+            op1 = 0.0;
+            op2 = 0.0;
+            DisplayNumber = "0";
+        }
+        private void ClearPartial()
+        {
+            currentResult = "";
+            DisplayNumber = "0";
+        }
+    }
+}
